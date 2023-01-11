8c5ae1d286946b56ca2ba5d151946d123131e55b
Daniel Jimenez <danieljimenezcanales@gmail.com>
2023-01-11 18:09:05 -0500

    finish landing page

---
+++ README.md
@@ -0,0 +1,42 @@
+# Sunnyside agency landing page
+
+![Design preview for the Sunnyside agency landing page](./design/desktop-preview.jpg)
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### Links
+
+- Solution URL: [Update Link](https://github.com/DanielJimenezC/sunnyside)
+- Live Site URL: [Update Link](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- HTML
+- CSS
+- JavaScript
+- Mobile-first workflow
+
+## Author
+
+- Website - [Daniel Jimenez](https://www.daniel-jimenez.tech)
+- Frontend Mentor - [@DanielJimenezC](https://www.frontendmentor.io/profile/DanielJimenezC)
+- Twitter - [@PolloWithPapas](https://twitter.com/PolloWithPapas)
+
+## Welcome! 👋
+
+Thanks for checking out this landing page.
+
+[Frontend Mentor](https://www.frontendmentor.io)
---
+++ index.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+  main()
+});
+
+const navButton = document.getElementById('nav-button'),
+      navMenu = document.getElementById('nav-menu')
+
+function load() {
+  const load = document.getElementById("load");
+  setTimeout(() => { 
+    load.classList.add("load-end") 
+  }, 2500)
+}
+
+function main() {
+  load()
+  if (navButton) {
+    navButton.addEventListener('click', () => {
+      const isOpen = !navMenu.classList.contains('open')
+      if (isOpen) {
+        navMenu.classList.toggle('open')
+       setTimeout(() => {
+        navMenu.classList.toggle('nav-menu-open')
+      }, 50);
+      } else {
+        navMenu.classList.toggle('nav-menu-open')
+        setTimeout(() => {
+          navMenu.classList.toggle('open')
+        }, 90);
+      }
+    })
+  }
+  window.addEventListener('scroll', onScrollNav)
+}
+
+function onScrollNav() {
+  const navbar = document.getElementsByClassName('header')[0]
+  if (this.scrollY >= 50) navbar.classList.add('header-scroll') 
+  else navbar.classList.remove('header-scroll')
+}
